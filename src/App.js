f1148eb8c624fc328e68e034d2f21f0521bae8bd
Eesha-Shahid <eesha.shahid14@gmail.comm>
2023-02-09 04:04:43 +0500

    Tutorial 6 completed.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import About from './components/About';
+import { useState } from 'react';
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
 function App(){
+
+  // Whether dark mode enabled or not
+  const [mode, setMode] = useState('light');
+  
+  const [alert,setAlert]= useState(null);
+
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+    }, 1500);
+  }
+
+  const toggleMode=()=>{
+    if (mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled","success");
+      document.title="TextUtils - Dark Mode";
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled","success");
+      document.title="TextUtils - Light Mode";
+    }
+  }
+
   return(
     <>
-    <Navbar title="Cosmic Cafe" aboutText="About Cosmic Cafe"/>
-    <div className="container my-3">
-      <TextForm heading="Enter the text to analyze"/>
-    </div>
+    <Router>
+      <Navbar title="Text Utils" aboutText="About Cosmic Cafe" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
+      <div className="container my-3">
+        <Routes>
+            <Route exact path ="/about" element={<About/>}/>
+            <Route exact path ="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze" mode={mode}/>}/>
+        </Routes>
+      </div>
+    </Router>
     </>
   );
 }
